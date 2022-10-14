02ccd2c0051487b5e0cc48214b089650da42b197
pnolle <pnolle@web.de>
2022-10-14 22:10:12 +0200

    cleanup

--- analyze.py
+++ analyze.py
@@ -2,25 +2,13 @@ import cv2
 import datetime
 import os
 
-# img = cv2.imread('2.png',1)
-# rows,cols,_ = img.shape
-
-# for i in range(rows):
-#     for j in range(cols):
-#         k = img[i,j]
-#         print(k)
-
-
 def video2Matrix(script_path, videofile, assetsfolder, dumpsfolder):
     print('video2Matrix', script_path, videofile, assetsfolder, dumpsfolder)
 
-    # cap = cv2.VideoCapture('VerticalLineLoop.mp4')
     cap = cv2.VideoCapture('{}/{}/{}'.format(script_path,assetsfolder,videofile))
-    # count the number of frames
     frames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
     fps = cap.get(cv2.CAP_PROP_FPS)
     
-    # calculate duration of the video
     seconds = round(frames / fps)
     video_time = datetime.timedelta(seconds=seconds)
     print(f'duration in seconds: {seconds}')
@@ -39,20 +27,6 @@ def video2Matrix(script_path, videofile, assetsfolder, dumpsfolder):
         print (counter, range(rows))
         f.write('####################### Frame #'+ str(counter) + '\n'+str(frame)+'\n')
         counter+=1
-    #     for i in range(rows):
-    #         k = frame[i]
-    #         print(i, k[0])
-            # for j in range(cols):
-            #     k = frame[i,j]
-            #     print(k)
-        # gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-        # cv.imshow('frame', gray)
-        # if cv.waitKey(1) == ord('q'):
-        #     break
-            
-    #     # Press Q on keyboard to exit
-    #         if cv2.waitKey(25) & 0xFF == ord('q'):
-    #                 break
 
     f.close()
     cap.release()
@@ -64,10 +38,3 @@ for f in os.listdir(script_path+'/assets'):
         print(f)
         print(script_path)
         video2Matrix(script_path, f, '/assets', '/dumps')
-
-
-# for file in os.listdir(os.path.join("/assets")):
-#     print(file)
-#     if file.endswith(".mp4"):
-#         video2Matrix(os.path.join("/assets")+file)
-#         # video2Matrix('mixkit-retro-80s-vhs-style-triangles-spin-over-neon-palm-tree-5391-medium.mp4')
