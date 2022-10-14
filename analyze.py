914132b092f0214b59b304bb0d299637faeeb930
pnolle <pnolle@web.de>
2022-10-14 23:44:32 +0200

    list of ndarrays

--- analyze.py
+++ analyze.py
@@ -1,11 +1,16 @@
 import cv2
 import datetime
 import os
+import numpy as np
+import json
 
 def video2Matrix(script_path, videofile, assetsfolder, dumpsfolder):
     print('video2Matrix', script_path, videofile, assetsfolder, dumpsfolder)
 
-    cap = cv2.VideoCapture('{}/{}/{}'.format(script_path,assetsfolder,videofile))
+    vfname = '{}/{}/{}'.format(script_path,assetsfolder,videofile)
+    tfname = '{}/{}/{}'.format(script_path,dumpsfolder,videofile+'.txt')
+
+    cap = cv2.VideoCapture(vfname)
     frames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
     fps = cap.get(cv2.CAP_PROP_FPS)
     
@@ -14,27 +19,33 @@ def video2Matrix(script_path, videofile, assetsfolder, dumpsfolder):
     print(f'duration in seconds: {seconds}')
     print(f'video time: {video_time}')
 
-    f = open(script_path+'/dumps/' + videofile + '.txt', 'w')
+    f = open(tfname, 'w')
 
     counter = 0
+    # frames = np.empty((1080, 1920, 3), np.uint8)
+    frames = []
+
     while cap.isOpened():
         ret, frame = cap.read()
         # if frame is read correctly ret is True
         if not ret:
             print('Can\'t receive frame (stream end?). Exiting ...')
             break
-        rows,cols,_ = frame.shape
-        print (counter, range(rows))
-        f.write('####################### Frame #'+ str(counter) + '\n'+str(frame)+'\n')
-        counter+=1
+        # rows,cols,_ = frame.shape
+        # f.write(str(frame))
+        # f.write('####################### Frame #'+ str(counter) + '\n'+str(frame)+'\n')
 
+        frames.append(frame)
+        counter+=1
+    print("ALL FRAMES", str(len(frames)), frames[0].shape)
+
+    f.write(str(frames))
     f.close()
+
     cap.release()
 
 script_path = os.path.dirname(os.path.abspath(__file__))
 
 for f in os.listdir(script_path+'/assets'):
     if f.endswith('.mp4'):
-        print(f)
-        print(script_path)
         video2Matrix(script_path, f, '/assets', '/dumps')
